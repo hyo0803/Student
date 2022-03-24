9530f7d3910c4853bb2748665833da0321396ad7
hyo0803 <90206886+hyo0803@users.noreply.github.com>
2022-03-24 18:18:47 +0300

    Update index.jsx

--- Hertek_5/index.jsx
+++ Hertek_5/index.jsx
@@ -1,28 +1,44 @@
-class TodoApp extends React.Component {  constructor(props) {
+class TodoApp extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = { items: [], text: '' };
+    this.state = { items: [], text: ''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
+    let date_ = new Date();
+    let mon = date_.getMonth();
+    let day = date_.getDate();
+    let months = [
+        'Jan', 'Feb', 'Mar', "Apr", 
+        "May", 'Jun', 'Jul', 'Aug',
+        'Sep', 'Oct', 'Nov', 'Dec'
+    ]
     return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="new-todo">
-            What needs to be done?
-          </label>
-          <input
-            id="new-todo"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button>
-            Add #{this.state.items.length + 1}
-          </button>
-        </form>
+      <div class="container-fluid">
+        <div class="dat">
+            <h3>{months[mon-1]} {day}st</h3>
+            <p>Good Day to Manage Your Deals!</p>
+        </div>
+        <div class="todo">
+            <h3>TODO</h3>
+            <TodoList items={this.state.items} />
+            <form onSubmit={this.handleSubmit}>
+            <label htmlFor="new-todo">
+                What needs to be done?
+            </label>
+            <input
+                id="new-todo"
+                onChange={this.handleChange}
+                value={this.state.text}
+                class="inp"
+            />
+            <button class="btn">
+                SUBMIT
+            </button>
+            </form>
+        </div>
       </div>
     );
   }
@@ -58,7 +74,8 @@ class TodoList extends React.Component {
     );
   }
 }
+
 ReactDOM.render(
-  <TodoApp />,
+  <TodoApp/>,
   document.getElementById('app')
-)
+);
